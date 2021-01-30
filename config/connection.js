9bd29450d29966fcf5fbddb0b3b54c7d6aa89bdd
jonesliddy77 <jonesliddy77@gmail.com>
2021-01-30 11:25:26 -0600

    fix

--- config/connection.js
+++ config/connection.js
@@ -2,29 +2,31 @@
 
 // * Export the connection.
 
+
 // Set up MySQL connection.
-var mysql = require('mysql');
-let connection;
+var mysql = require("mysql");
+let connection; 
+
 
 if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-        host: process.env.DB_HOST,
-        port: 3306,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: 'burgers_db',
-    });
-}
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'jonesliddy77',
+    database: 'burgers_db',
+  });
+};
 
 // Make connection.
-connection.connect(function (err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
